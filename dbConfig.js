7ea19cfa4948b28e376f738b8917baf80767d8ac
Ritsuki KOKUBO <ritsu2891@gmail.com>
2024-12-30 00:29:59 +0900

    DB関連の処理を実装

---
+++ dbConfig.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+
+module.exports = dbConfig;
